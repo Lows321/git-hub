52c17838c2c69b2d5c3655331158084b6f58f1ab
Lavr Stepanov <229196-23>
2023-05-22 13:16:29 +0300

    Initial commit

---
+++ лаб 11.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+using namespace std;
+
+template <typename T>
+T findel(const T a[], int size, const T el) {
+
+	for (int i = 0; i < size; i++) {
+		if (a[i] == el) {
+			return i;
+		}
+	}
+	return -1;
+}
+int main() {
+	setlocale(0, "Russian");
+	int a[] = { 5,6,12,9,8,4,6,7 };
+	int el;
+	int size = sizeof(a) / sizeof(a[0]);
+	cout << "Массив a : "<< endl;
+	for (int i = 0; i < size; i++) {
+		cout << a[i] << "\t";
+	}
+	cout <<endl<< "Введите элемент для поиска "<<endl;	
+	cin >> el;
+	int num = findel(a, size, el);
+	if (num == -1) {
+		cout << "Такого числа нет " << endl;
+	}
+	else {
+		cout << "Номер искомого числа = " << num + 1;
+	}
+}
---
+++ лабораторная 10.1.cpp
@@ -0,0 +1,53 @@
+﻿#include<iostream>
+#include <time.h>
+using namespace std;
+int i = 0;
+int j = 0;
+int alfa;
+int a[15];
+int b[15];
+int cnta = 0;
+int cntb = 0;
+void func() {
+	for (int i = 0; i < 15; i++) {
+
+		if (a[i] > alfa and b[i] > alfa) {
+			cnta ++;
+			cntb++;
+			continue;
+		}
+		else if (a[i] > alfa) {
+			cnta++;
+			continue;
+		}
+		else if (b[i] > alfa) {
+			cntb++;
+			continue;
+		}
+	}
+}
+int main() {
+	setlocale(0, "Russian");
+	srand(time(NULL) + rand());
+	cout << "Введите число alfa " << endl;
+	cin >> alfa;
+	for (int i=0; i < 15; i++) {
+		a[i] = rand() % 201 - 100;
+	}
+	for (int i=0; i < 15; i++) {
+		b[i] = rand() % 201 - 100;
+	}
+	cout << "Массив a:" << endl;
+	for (int i=0; i < 15; i++) {
+		cout << a[i] << " ";
+	}
+	cout <<endl<< "Массив b:" << endl;
+	for (int i=0; i < 15; i++) {
+		cout << b[i] << " ";
+	}
+	for (int i = 0; i < 15; i++) {
+	}
+	func();
+	cout <<endl <<"Количество элементов, которые больше alfa, в массиве a = " << cnta;
+	cout << endl << "Количество элементов, которые больше alfa, в массиве b = " << cntb;
+}
